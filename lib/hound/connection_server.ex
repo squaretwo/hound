58c0f4af125cc4cb2640ba290750f087bb9cc066
Donald Plummer <donald@squaretwo.co>
2018-09-25 10:57:03 -0700

    Add functions to update config info
    Because the path to get the correct host and port to the `get_url`
    function is too damn convoluted for me to follow right now.

--- lib/hound/connection_server.ex
+++ lib/hound/connection_server.ex
@@ -2,6 +2,35 @@ defmodule Hound.ConnectionServer do
   @moduledoc false
 
   def start_link(options \\ []) do
+    Agent.start_link(fn ->
+      %{
+        driver_info: load_driver_info(options),
+        configs: load_configs(options)
+      }
+    end, name: __MODULE__)
+  end
+
+  def driver_info do
+    {:ok, Agent.get(__MODULE__, &(&1.driver_info))}
+  end
+
+  def configs do
+    {:ok, Agent.get(__MODULE__, &(&1.configs))}
+  end
+
+  def put_in_configs(key, value) do
+    {:ok, Agent.update(__MODULE__, fn %{configs: configs} = state ->
+      %{state | configs: Map.put(configs, key, value)}
+    end)}
+  end
+
+  def put_in_driver_info(key, value) do
+    {:ok, Agent.update(__MODULE__, fn %{driver_info: driver_info} = state ->
+      %{state | driver_info: Map.put(driver_info, key, value)}
+    end)}
+  end
+
+  defp load_driver_info(options) do
     driver = options[:driver] || Application.get_env(:hound, :driver, "selenium")
 
     {default_port, default_path_prefix, default_browser} = case driver do
@@ -13,35 +42,24 @@ defmodule Hound.ConnectionServer do
         {4444, "wd/hub/", "firefox"}
     end
 
-
     browser = options[:browser] || Application.get_env(:hound, :browser, default_browser)
     host = options[:host] || Application.get_env(:hound, :host, "http://localhost")
     port = options[:port] || Application.get_env(:hound, :port, default_port)
     path_prefix = options[:path_prefix] || Application.get_env(:hound, :path_prefix, default_path_prefix)
 
-
-    driver_info = %{
-      :driver => driver,
-      :browser => browser,
-      :host => host,
-      :port => port,
-      :path_prefix => path_prefix
+    %{
+      driver: driver,
+      browser: browser,
+      host: host,
+      port: port,
+      path_prefix: path_prefix
     }
-
-    configs = %{
-      :host => options[:app_host] || Application.get_env(:hound, :app_host, "http://localhost"),
-      :port => options[:app_port] || Application.get_env(:hound, :app_port, 4001)
-    }
-
-    state = %{sessions: [], driver_info: driver_info, configs: configs}
-    Agent.start_link(fn -> state end, name: __MODULE__)
   end
 
-  def driver_info do
-    {:ok, Agent.get(__MODULE__, &(&1.driver_info), 60000)}
-  end
-
-  def configs do
-    {:ok, Agent.get(__MODULE__, &(&1.configs), 60000)}
+  defp load_configs(options) do
+    %{
+      host: options[:app_host] || Application.get_env(:hound, :app_host, "http://localhost"),
+      port: options[:app_port] || Application.get_env(:hound, :app_port, 4001)
+    }
   end
 end
